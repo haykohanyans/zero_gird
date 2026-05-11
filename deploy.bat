444b14900909b20dc730f7ad26b8bd1f3fb033a6
haykohanyans <hvohanyan@gmail.com>
2026-05-11 20:06:24 +0400

    Syncing local changes

--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,9 @@
 echo Deploying to GitHub...
 git add .
 git commit -m "Update game (English version + bug fixes)"
+echo Pulling latest changes from GitHub...
+git pull origin main --rebase
+echo Pushing to GitHub...
 git push origin main
 echo Done!
 pause
